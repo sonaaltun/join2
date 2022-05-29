0b197c942e8c28154afbaafdf7f144a8f7a4ff7c
Sona Altun <74629475+sonaaltun@users.noreply.github.com>
2022-05-29 16:09:36 +0300

    Create ödev10_3.sql

---
+++ ödev10_3.sql
@@ -0,0 +1,6 @@
+--customer tablosu ile rental tablosunda bulunan rental_id ile customer 
+--tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz FULL JOIN sorgusunu yazınız.
+SELECT first_name, last_name, rental.rental_id
+FROM customer
+RIGHT JOIN rental
+ON customer.customer_id = rental.customer_id;
