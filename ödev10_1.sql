3ea93f78861f9746b8fa48b520a455cf5f869c28
Sona Altun <74629475+sonaaltun@users.noreply.github.com>
2022-05-29 16:17:54 +0300

    Update ödev10_1.sql

--- ödev10_1.sql
+++ ödev10_1.sql
@@ -1,4 +1,4 @@
 --city tablosu ile country tablosunda bulunan şehir 
 --(city) ve ülke (country) isimlerini birlikte görebileceğimiz LEFT JOIN sorgusunu yazınız.
 select city, country from city
-left join country on country.country_id = city.country_id;
+left join country on city.country_id = country.country_id;
