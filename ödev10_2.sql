b4329aaf1b95ce33265d34724229e0f88a9c5a0d
Sona Altun <74629475+sonaaltun@users.noreply.github.com>
2022-05-29 16:07:39 +0300

    Create ödev10_2.sql

---
+++ ödev10_2.sql
@@ -0,0 +1,6 @@
+--customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki 
+--first_name ve last_name isimlerini birlikte görebileceğimiz RIGHT JOIN sorgusunu yazınız.
+SELECT first_name, last_name, payment.payment_id
+FROM customer
+RIGHT JOIN payment
+ON customer.customer_id = payment.customer_id;
